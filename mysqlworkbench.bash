ab84835dca12e414c23efb2206f8da31df5501e6
Gerardo <gerardorf@gmail.com>
2021-10-14 18:56:55 +0200

    run mySQLWorkbench from command line

---
+++ mysqlworkbench.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# remap caps lock to control
+#
+
+function mySQLWorkbench() {
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    open -na "MySQLWorkbench.app" --args "$@"
+  fi
+}
